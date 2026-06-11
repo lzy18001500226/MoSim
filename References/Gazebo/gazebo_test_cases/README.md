74bbc74b9672a1586d49d11a337ab1f5e722cff5
lzy18001500226 <lzy18001500226@users.noreply.github.com>
2026-06-11 13:55:35 +0800

    refs: import gazebosim gazebo_test_cases snapshot

---
+++ References/Gazebo/gazebo_test_cases/README.md
@@ -0,0 +1,126 @@
+# Gazebo Jetty Tutorial Party
+
+Welcome! The purpose of this repository is to guide the testing of various functionalities of Gazebo Jetty prior to official release.
+In other words, welcome to the Tutorial Party!
+
+> [!NOTE]
+> # 🏆 [View the live leaderboard](https://github.com/gazebosim/gazebo_test_cases/blob/leaderboard/README.md) 🏆
+
+
+## Who can contribute?
+
+Everyone, all contributions are welcome!
+
+### Don't have a compatible platform?
+
+Jetty on Ubuntu is only supported on the Noble version of Ubuntu (24.04). If you don't have this platform,
+there are [docker images](https://github.com/gazebosim/gz-jetty/pkgs/container/gz-jetty) ready to use.
+
+We recommend installing
+[`rocker`](https://github.com/osrf/rocker?tab=readme-ov-file#installation) for
+running the docker image since it handles X11 and GPU configurations for you.
+Once installed, you can run
+
+```bash
+rocker  --x11 --  ghcr.io/gazebosim/gz-jetty:main
+```
+
+to start `bash` inside a `gz-jetty` container. Then you can run any of the
+Gazebo commands in the tutorial e.g., `gz sim -v4 shapes.sdf`. Alternatively,
+you can run Gazebo directly through a `rocker` one-liner.
+
+```bash
+rocker  --x11 --  ghcr.io/gazebosim/gz-jetty:main gz sim -v4 shapes.sdf
+```
+
+> [!TIP]
+> If you have an NVIDIA graphics card, you'll need to add `--nvidia` to the `rocker` command. See `rocker` [README](https://github.com/osrf/rocker?tab=readme-ov-file#ros-2-rviz).
+
+> [!TIP]
+> If you have an Intel integrated graphics card, you'll need to add `--devices /dev/dri` to the `rocker` command.
+> See `rocker` [README](https://github.com/osrf/rocker?tab=readme-ov-file#installation).
+
+## How to contribute?
+
+The first step is to register on our sign up [form](https://docs.google.com/forms/d/e/1FAIpQLSc1tHoqcYQcuLG4b_ao2uA9cGdX4huNBXyuqd10wI0KT-GqSw/viewform) so
+we can map your Github username to your email so we can send you instructions on getting your free swag if you are in the top 20.
+
+> [!NOTE]
+> More detailed instructions are available on our Discourse [post](https://discourse.openrobotics.org/t/gazebo-jetty-test-and-tutorial-party-instructions/49779)
+
+The [Issues](https://github.com/gazebosim/gazebo_test_cases/issues) page has several tickets each containing specific instructions to test a particular functionality of Gazebo Jetty.
+Each ticket will have the following sections:
+
+1. **Setup**: Details on the desired hardware and software setup for this test. The following combinations are possible.
+   1. BuildType:
+      - `binary`: pre-built Debian packages from the apt repository
+      - `source`: building your own workspace from source
+   2. Os: `Ubuntu Noble`, `Windows` and `MacOS`
+2. **Links**: Any relevant references for this test.
+3. **Checks**: A list of functionalities to validate.
+
+To contribute, first ensure if you have the relevant setup as described in the ticket.
+If you don't have the setup, you can find the installation instructions at <https://gazebosim.org/docs/jetty/install>.
+Next, comment on the ticket indicating your interest so that others will know
+you are working on it.
+Then follow the instructions to perform the necessary checks.
+
+**All checks passed?**
+Great! Attaching your terminal output (codeblock comment or gist file) or a screenshot as a form of verification is greatly appreciated.
+At the end of your comment, add a specially formatted text, `[status: passed]` to let us know that it passed.
+
+**Encountered failures?**
+If one or more checks fail, please provide the error message in a codeblock comment or as a gist file attachment.
+At the end of your comment, add a specially formatted text, `[status: failed]` to let us know that it failed.
+
+## Windows Tests
+
+Binaries for the prereleases are not available for Windows, therefore, it is necessary to build
+all Gazebo packages from source following the [Install from source](https://gazebosim.org/docs/jetty/install_windows_src/) instructions.
+
+## MacOS Tests
+
+With the introduction of standalone executables, you should now be to run `gz
+sim` without starting the server and the GUI separately.
+
+## What if I want to test something else?
+
+If you would like to test the functionality of any other package or extend the capabilities tested above, please reach out to us on [Discord](bit.ly/GazeboDiscord) in the in the  `#gazebo-help` channel.
+
+## Questions
+
+Questions related to testing may be posted on [Discord](bit.ly/GazeboDiscord) in the in the  `#gazebo-help` channel. Kindly ensure to link the relevant issue ticket when starting a new discussion.
+
+### Tickets filtered by Setup
+
+- [`buildType: Binary`, `os: Ubuntu Noble`](https://github.com/gazebosim/gazebo_test_cases/issues?q=is:issue+is:open+label:%22buildType:+Binary%22+label:%22os:+Ubuntu+Noble%22)
+- [`buildType: Binary`, `os: MacOS`](https://github.com/gazebosim/gazebo_test_cases/issues?q=is:issue+is:open+label:%22buildType:+Binary%22+label:%22os:+MacOS%22)
+- [`buildType: Source`, `os: Ubuntu Noble`](https://github.com/gazebosim/gazebo_test_cases/issues?q=is:issue+is:open+label:%22buildType:+Source%22+label:%22os:+Ubuntu+Noble%22)
+- [`buildType: Source`, `os: MacOS`](https://github.com/gazebosim/gazebo_test_cases/issues?q=is:issue+is:open+label:%22buildType:+Source%22+label:%22os:+MacOS%22)
+- [`buildType: Source`, `os: Windows`](https://github.com/gazebosim/gazebo_test_cases/issues?q=is:issue+is:open+label:%22buildType:+Source%22+label:%22os:+Windows%22)
+- [`os: Ubuntu Noble`](https://github.com/gazebosim/gazebo_test_cases/issues?q=is:issue+is:open+label:%22os:+Ubuntu+Noble%22)
+- [`os: MacOS`](https://github.com/gazebosim/gazebo_test_cases/issues?q=is:issue+is:open+label:%22os:+MacOS%22)
+- [`os: Windows`](https://github.com/gazebosim/gazebo_test_cases/issues?q=is:issue+is:open+label:%22os:+Windows%22)
+- [`os: Any`](https://github.com/gazebosim/gazebo_test_cases/issues?q=is:issue+is:open+label:%22os:+Any%22)
+
+#### Zeromq and Zenoh
+
+- [`os: Ubuntu Noble`, `transport: zeromq`](https://github.com/gazebosim/gazebo_test_cases/issues?q=is:issue+is:open+label:%22os:+Ubuntu+Noble%22+label:%22transport:+zeromq%22)
+- [`os: MacOS`, `transport: zeromq`](https://github.com/gazebosim/gazebo_test_cases/issues?q=is:issue+is:open+label:%22os:+MacOS%22+label:%22transport:+zeromq%22)
+- [`os: Windows`, `transport: zeromq`](https://github.com/gazebosim/gazebo_test_cases/issues?q=is:issue+is:open+label:%22os:+Windows%22+label:%22transport:+zeromq%22)
+
+- [`os: Ubuntu Noble`, `transport: zenoh`](https://github.com/gazebosim/gazebo_test_cases/issues?q=is:issue+is:open+label:%22os:+Ubuntu+Noble%22+label:%22transport:+zenoh%22)
+- [`os: MacOS`, `transport: zenoh`](https://github.com/gazebosim/gazebo_test_cases/issues?q=is:issue+is:open+label:%22os:+MacOS%22+label:%22transport:+zenoh%22)
+- [`os: Windows`, `transport: zenoh`](https://github.com/gazebosim/gazebo_test_cases/issues?q=is:issue+is:open+label:%22os:+Windows%22+label:%22transport:+zenoh%22)
+
+#### Filter based on completion status
+
+- [`not-started`](https://github.com/gazebosim/gazebo_test_cases/issues?q=is%3Aissue%20state%3Aopen%20label%3Anot-started)
+- [no comments (not claimed)](https://github.com/gazebosim/gazebo_test_cases/issues?q=is%3Aissue%20state%3Aopen%20label%3Anot-started%20sort%3Acomments-asc)
+---
+
+Here are some things to know about Github search:
+
+- You can filter available results with a label by typing `label:my-label`. And it works for multiple labels: `label:my-label-1 label:my-label-2`.
+- If your label has a space in it, you can put the label in quotes: `label:"my label"`.
+- To show results that **don't** have a label, use a minus sign: `-label:no-results-with-this-label`.
---
+++ References/Gazebo/gazebo_test_cases/leader_board.py
@@ -0,0 +1,227 @@
+from github import Github
+import os
+import jinja2
+import argparse
+
+valid_labels = ["not-started", "completion-1", "completion-2", "completion-3"]
+score_multiplier_labels = { "os: Windows":  1.2, "os: MacOS": 1.2}
+score_difficulty_labels = { "easy":  6, "hard": 18}
+needs_review_labels = ["failed", "fixed"]
+needs_review_emoji = [":x:", ":negative_squared_cross_mark:"]
+completion_keywords = ["[status: passed]", "[status: failed]"]
+score_board = {}
+
+leaderboard_template = r"""
+# **LEADER BOARD**
+
+| **Place** | **User** | **Points** | **Contributions** |
+| :-------: | :-------: | :------: | :-------: |
+{% for user in users -%}
+| {{ loop.index }} | <img src="{{ users[user].avatar_url }}" alt="{{ user }}" width="128" height="128"> <br> [{{ user }}](https://github.com/{{ user }}) <br> **Unique Completed: {{ users[user].number_unique_completed }}** <br> $${{ '{' }}\color{{ '{' }}green{{ '}' }}First \space Completed: \space {{ users[user].number_first_completed }}{{ '}' }}$$ <br> $${{ '{' }}\color{{ '{' }}yellow{{ '}' }}Second \space Completed: \space {{ users[user].number_second_completed }}{{ '}' }}$$ <br> $${{ '{' }}\color{{ '{' }}red{{ '}' }}Third \space Completed: \space {{ users[user].number_third_completed }}{{ '}' }}$$ <br> Total Completed: {{ users[user].number_completed }} | {{ users[user].score|round(2) }}  | {% for issue in users[user].issues %}[#{{ issue }} +{{ users[user]['issues'][issue].points|round(2) }}]({{ users[user]['issues'][issue].comment_url }}) $${{ '{' }}\color{{ '{' }}{%- if  users[user]['issues'][issue].completer_number == 1 -%}green{%- elif  users[user]['issues'][issue].completer_number == 2 -%}yellow{%- else -%}red{%- endif -%}{{ '}' }}({{ users[user]['issues'][issue].completer_number }}/{{ users[user]['issues'][issue].number_completions}}){{ '}' }}$${{ users[user]['issues'][issue].emoji }}  {% if loop.index % 3 == 0 %}<br>{% endif %}{% endfor %} |
+{% endfor %}
+"""
+
+simple_leaderboard_template = r"""
+# **LEADER BOARD**
+
+| **Place** | **User** | **Points** |
+| :-------: | :------- | :------: |
+{% for user in users -%}
+| {{ loop.index }} | <img src="{{ users[user].avatar_url }}" alt="{{ user }}" width="24" height="24">&emsp;**{{ user }}** | {{ users[user].score|round(2) }} |
+{% endfor %}
+"""
+
+template = jinja2.Template(leaderboard_template)
+simple_template = jinja2.Template(simple_leaderboard_template)
+
+g = Github(os.getenv("GITHUB_TOKEN"))
+
+def get_repo_object(owner, repo_name):
+    try:
+        repo = g.get_user(owner).get_repo(repo_name)
+        return repo
+    except Exception as e:
+        print(f"Error: {e}")
+        return []
+
+def get_issue_object(repo, issue_number):
+    try:
+        issue = repo.get_issue(number=issue_number)
+        return issue
+    except Exception as e:
+        print(f"Error: {e}")
+        return []
+
+def get_issue_details(issue, issue_number):
+    try:
+        comments_data = []
+        label_data = []
+        assignee_data = []
+        ignore_completer = ["caguero", "mjcarroll", "iche033", "ahcorde", "jennuine", "bperseghetti", "scpeters", "jrivero", "traversaro", "azeey"]
+        unique_completer = []
+        for assignee in issue.assignees:
+            assignee_data.append(assignee.login)
+        for label in issue.get_labels():
+            if (label.name in valid_labels or
+                label.name in score_multiplier_labels or
+                label.name in score_difficulty_labels or
+                label.name in needs_review_labels):
+                label_data.append(label.name)
+        for comment in issue.get_comments():
+            sanitized_comment = comment.body.replace(f"> {completion_keywords[0]}","").replace(f"> {completion_keywords[1]}","")
+            if (completion_keywords[0] in sanitized_comment or
+                completion_keywords[1] in sanitized_comment):
+                if (comment.user.login not in unique_completer and
+                    comment.user.login not in ignore_completer):
+                    print(f"Issue: #{issue_number} - Adding completer: {comment.user.login}")
+                    unique_completer.append(comment.user.login)
+                    comments_data.append({
+                        'user': comment.user.login,
+                        'body': comment.body,
+                        'url': comment.html_url
+                    })
+                else:
+                    print(f"Issue: #{issue_number} - Ignoring completer: {comment.user.login}")
+        return comments_data, label_data, assignee_data, issue.title
+    except Exception as e:
+        print(f"Error: {e}")
+        return []
+
+def evaluate_completions (comment_completion, issue_labels, issue_assignees, issue_title, issue_number, issue):
+        score_multiplier = 1
+        score_base_value = 12
+        unique_completion = 1
+        for score_label in issue_labels:
+            if score_label in score_multiplier_labels:
+                score_multiplier = score_multiplier_labels[score_label]
+                print(f"Issue: #{issue_number} - label: { score_label } modifying multiplier: {score_multiplier} ")
+            if score_label in score_difficulty_labels:
+                score_base_value = score_difficulty_labels[score_label]
+                print(f"Issue: #{issue_number} - label: { score_label } modifying difficulty value: {score_base_value} ")
+        score_value = score_multiplier * score_base_value
+        number_completions = len(comment_completion)
+        if number_completions > 1:
+            unique_completion = 0
+        if number_completions > 3:
+            print(f"Issue: #{issue_number} -  exceeds limit of 3 completers with value of {number_completions}.")
+            comment_completion=comment_completion[:3]
+            number_completions = 3
+        if number_completions <= 3:
+            if valid_labels[number_completions] not in issue_labels:
+                for label in issue_labels:
+                    if label in valid_labels:
+                        print(f"Issue: #{issue_number} - Removing label: {label}")
+                        issue.remove_from_labels(label)
+                print(f"Issue: #{issue_number} - Adding label: {valid_labels[number_completions]}")
+                issue.add_to_labels(valid_labels[number_completions])
+            completer_number=0
+            for comment in comment_completion:
+                completer_number+=1
+                completion_emoji="  "
+                if completion_keywords[1] in comment["body"]:
+                    if needs_review_labels[0] not in issue_labels:
+                        issue.add_to_labels(needs_review_labels[0])
+                        completion_emoji=needs_review_emoji[0]
+                        print(f"Issue: #{issue_number} - completed as failed, label added")
+                    elif needs_review_labels[1] in issue_labels:
+                        completion_emoji=needs_review_emoji[1]
+                        print(f"Issue: #{issue_number} - completed as failed but fix confirmed")
+                    else:
+                        completion_emoji=needs_review_emoji[0]
+                        print(f"Issue: #{issue_number} - completed as failed, no fix yet")
+                if comment["user"] not in issue_assignees:
+                    print(f"Issue: #{issue_number} - Adding assignee: {comment["user"]}")
+                    issue.add_to_assignees(comment["user"])
+                if comment['user'] not in score_board:
+                    score_board[comment['user']] = {
+                        "issues": {},
+                        "score": score_value/number_completions,
+                        "avatar_url": g.get_user(comment['user']).avatar_url,
+                        "number_completed": 1,
+                        "number_unique_completed": unique_completion,
+                        "number_first_completed": 1 if completer_number == 1 else 0 ,
+                        "number_second_completed": 1 if completer_number == 2 else 0 ,
+                        "number_third_completed": 1 if completer_number == 3 else 0
+                        }
+                    score_board[comment['user']]['issues'][issue_number] = {
+                        "comment_url": comment["url"],
+                        "points": score_value/number_completions,
+                        "emoji": completion_emoji,
+                        "completer_number": completer_number,
+                        "number_completions": number_completions
+                        }
+                else:
+                    score_board[comment['user']]['issues'][issue_number]={
+                        "comment_url": comment["url"],
+                        "points": score_value/number_completions,
+                        "emoji": completion_emoji,
+                        "completer_number": completer_number,
+                        "number_completions": number_completions
+                    }
+                    score_board[comment['user']]['score']+=score_value/number_completions
+                    score_board[comment['user']]['number_completed']+=1
+                    score_board[comment['user']]['number_first_completed']+=1 if completer_number == 1 else 0
+                    score_board[comment['user']]['number_second_completed']+=1 if completer_number == 2 else 0
+                    score_board[comment['user']]['number_third_completed']+=1 if completer_number == 3 else 0
+                    score_board[comment['user']]['number_unique_completed']+=unique_completion
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "--owner", type=str, default="gazebosim", help="Owner of the repository")
+    parser.add_argument("-r", "--repository", type=str, default="gazebo_test_cases", help="Repository to score issues over")
+    parser.add_argument("-l", "--label", type=str, default="ionic", help="Label to get relevant issues from")
+    args = parser.parse_args()
+
+    repo_owner = args.owner
+    repository_name = args.repository
+    target_label = args.label
+    i=0
+    target_repo = get_repo_object(repo_owner, repository_name)
+
+    repo_issues_object = list(target_repo.get_issues(state="all", labels=[target_label]))
+    num_issues = len(repo_issues_object)
+    for issue_object in repo_issues_object:
+
+        target_issue_number = issue_object.number
+        i+=1
+        print(f"Processing #{target_issue_number} - ({i}/{num_issues})")
+
+
+        comments, labels, assignees, title = get_issue_details(issue_object, target_issue_number)
+
+        if labels and comments:
+            evaluate_completions(comments, labels, assignees, title, target_issue_number, issue_object)
+
+    sorted_score_board = dict(sorted(score_board.items(), key=lambda item: item[1]["score"], reverse=True))
+
+    leader_board_md = template.render(users=sorted_score_board)
+    try:
+        file_name = f"{target_label.upper()}_LEADER_BOARD.md"
+        branch_name="leaderboard"
+        contents = target_repo.get_contents(file_name, ref=branch_name)
+        target_repo.update_file(contents.path, f"Update {file_name}", leader_board_md, contents.sha, branch=branch_name)
+        print(f"{file_name} UPDATED")
+    except Exception:
+        target_repo.create_file(file_name, f"Update {file_name}", leader_board_md, branch=branch_name)
+        print(f"{file_name} CREATED")
+    try:
+        file_name_readme = "README.md"
+        branch_name="leaderboard"
+        contents = target_repo.get_contents("README.md", ref=branch_name)
+        target_repo.update_file(contents.path, "Update README.md", leader_board_md, contents.sha, branch=branch_name)
+        print("README.md UPDATED")
+    except Exception:
+        target_repo.create_file("README.md", f"Update README.md", leader_board_md, branch=branch_name)
+        print("README.md CREATED")
+
+    simple_leader_board_md = simple_template.render(users=sorted_score_board)
+    try:
+        file_name = f"{target_label.upper()}_SIMPLE_LEADER_BOARD.md"
+        branch_name="leaderboard"
+        contents = target_repo.get_contents(file_name, ref=branch_name)
+        target_repo.update_file(contents.path, f"Update {file_name}", simple_leader_board_md, contents.sha, branch=branch_name)
+        print(f"{file_name} UPDATED")
+    except Exception:
+        target_repo.create_file(file_name, f"Update {file_name}", simple_leader_board_md, branch=branch_name)
+        print(f"{file_name} CREATED")
