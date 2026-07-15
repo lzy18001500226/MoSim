51a231be382393754b363664c23d45c9bb54113e
lzy18001500226 <lzy18001500226@users.noreply.github.com>
2026-07-15 19:39:05 +0800

    chore(references): add gz transport logging sources

---
+++ References/Gazebo/gz-transport/log/src/cmd/LogCommandAPI.cc
@@ -0,0 +1,158 @@
+/*
+ * Copyright (C) 2017 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#include "LogCommandAPI.hh"
+
+#include <csignal>
+#include <filesystem>
+#include <iostream>
+#include <regex>
+#include <string>
+
+#include <gz/transport/log/Export.hh>
+#include <gz/transport/log/Playback.hh>
+#include <gz/transport/log/Recorder.hh>
+#include <gz/transport/Node.hh>
+#include <gz/transport/NodeOptions.hh>
+#include "../Console.hh"
+
+using namespace gz;
+transport::log::PlaybackHandlePtr g_playbackHandler;
+
+//////////////////////////////////////////////////
+void verbosity(const int _level)
+{
+  if (_level < 0 || _level > 4)
+  {
+    std::cerr << "Invalid verbosity level\n";
+  }
+  transport::log::__verbosity = _level;
+}
+
+//////////////////////////////////////////////////
+void recordTopics(const char *_file, const char *_pattern, bool _force)
+{
+  if (_force)
+  {
+    try
+    {
+      std::filesystem::remove(_file);
+      LDBG("Deleted existing file.\n");
+    }
+    catch(const std::filesystem::filesystem_error &e)
+    {
+      LERR("Unable to delete existing file.\n");
+      return;
+    }
+  }
+
+  std::regex regexPattern;
+  try
+  {
+    regexPattern = _pattern;
+  }
+  catch (const std::regex_error &e)
+  {
+    LERR("Regex pattern is invalid\n");
+    return;
+  }
+
+  transport::log::Recorder recorder;
+
+  if (recorder.AddTopic(regexPattern) < 0)
+    return;
+
+  if (recorder.Start(_file) != transport::log::RecorderError::SUCCESS)
+    return;
+
+  // Wait until signaled (SIGINT, SIGTERM)
+  transport::waitForShutdown();
+  LDBG("Shutting down\n");
+  recorder.Stop();
+}
+
+//////////////////////////////////////////////////
+void playbackSignHandler(int) // NOLINT
+{
+  g_playbackHandler->Stop();
+}
+
+//////////////////////////////////////////////////
+void playbackTopics(const char *_file, const char *_pattern,
+    const int _wait_ms, const char *_remap, int _fast)
+{
+  std::regex regexPattern;
+  try
+  {
+    regexPattern = _pattern;
+  }
+  catch (const std::regex_error &e)
+  {
+    LERR("Regex pattern is invalid\n");
+    return;
+  }
+
+  // Parse remapping.
+  transport::NodeOptions nodeOptions;
+  std::string remap = std::string(_remap);
+  if (!remap.empty())
+  {
+    // Sanity check: It should contain the := delimiter.
+    auto delim = remap.find(":=");
+    if (delim == std::string::npos)
+    {
+      LERR("Invalid remap as := delimiter is missing\n");
+      return;
+    }
+
+    std::string from = remap.substr(0, delim);
+    std::string to = remap.substr(delim + 2, remap.size() - delim - 1);
+
+    if (!nodeOptions.AddTopicRemap(from, to))
+    {
+      LERR("Invalid remap of topics\n");
+      return;
+    }
+  }
+
+  transport::log::Playback player(_file, nodeOptions);
+  if (!player.Valid())
+    return;
+
+  if (player.AddTopic(regexPattern) < 0)
+  {
+    LERR("Failed to advertise topic(s)\n");
+    return;
+  }
+
+  std::this_thread::sleep_for(std::chrono::milliseconds(_wait_ms));
+
+  std::signal(SIGINT, playbackSignHandler);
+  std::signal(SIGTERM, playbackSignHandler);
+
+  if (_fast)
+    g_playbackHandler = player.Start(std::chrono::seconds(1), false);
+  else
+    g_playbackHandler = player.Start(std::chrono::seconds(1), true);
+
+  if (!g_playbackHandler)
+    return;
+
+  // Wait until playback finishes
+  g_playbackHandler->WaitUntilFinished();
+  LDBG("Shutting down\n");
+}
---
+++ References/Gazebo/gz-transport/log/src/cmd/LogCommandAPI.hh
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2018 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#include <gz/transport/log/Export.hh>
+
+/// \brief Sets verbosity of library
+/// \param[in] _level [0-4] Verbosity level
+void verbosity(const int _level);
+
+/// \brief Record topics whose name matches the given pattern
+/// \param[in] _file Path to the log file to record
+/// \param[in] _pattern ECMAScript regular expression to match against topics
+/// \param[in] _force Flag to overwrite existing file
+void recordTopics(
+  const char *_file,
+  const char *_pattern,
+  bool _force);
+
+/// \brief Playback topics whose name matches the given pattern
+/// \param[in] _file Path to the log file to playback
+/// \param[in] _pattern ECMAScript regular expression to match against topics
+/// \param[in] _wait_ms How long to wait before the publications begin after
+/// advertising the topics that will be played back (milliseconds)
+/// \param[in] _fast Set to > 0 to disable wait between messages.
+void playbackTopics(
+  const char *_file,
+  const char *_pattern,
+  const int _wait_ms,
+  const char *_remap,
+  int _fast);
