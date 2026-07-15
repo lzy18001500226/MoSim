827c40322d02172a8e7cd5ace5051a6a1d2c07e5
lzy18001500226 <lzy18001500226@users.noreply.github.com>
2026-07-16 05:48:44 +0800

    refs(fast-racing): remove old location batch 12

--- References/Lab/Fast-Racing/zhangjiajie.sh
+++
@@ -1,6 +0,0 @@
-roslaunch plan_manage zhangjiajie.launch     & sleep 1;
-roslaunch traj_server traj_server.launch & sleep 1;
-roslaunch Ctrl ctrl_md.launch  & sleep 1;
-roslaunch plan_manage visualize.launch;
-wait
-
