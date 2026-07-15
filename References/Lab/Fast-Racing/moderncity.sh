ce40109a8df24cfc23bdc0025c3e7b34dbd55ac7
lzy18001500226 <lzy18001500226@users.noreply.github.com>
2026-07-16 05:40:43 +0800

    refs(fast-racing): remove old location batch 01

--- References/Lab/Fast-Racing/moderncity.sh
+++
@@ -1,6 +0,0 @@
-roslaunch plan_manage moderncity.launch     & sleep 1;
-roslaunch traj_server traj_server.launch & sleep 1;
-roslaunch Ctrl ctrl_md.launch  & sleep 1;
-roslaunch plan_manage visualize.launch;
-wait
-
