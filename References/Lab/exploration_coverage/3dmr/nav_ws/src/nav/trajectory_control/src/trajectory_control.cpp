bed3acc26a0c76c206847c02739b8454faf9661c
lzy18001500226 <lzy18001500226@users.noreply.github.com>
2026-07-16 08:19:53 +0800

    refs(3dmr): add robotics source batch 15

---
+++ References/Lab/exploration_coverage/3dmr/nav_ws/src/nav/trajectory_control/src/trajectory_control.cpp
@@ -0,0 +1,34 @@
+/**
+* This file is part of the ROS package trajectory_control which belongs to the framework 3DMR. 
+*
+* Copyright (C) 2016-present Luigi Freda <luigifreda at gmail dot com> and Alcor Lab (La Sapienza University)
+* For more information see <https://github.com/luigifreda/3dmr>
+*
+* 3DMR is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* 3DMR is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with 3DMR. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <math.h> 
+#include <TrajectoryControlActionServer.h>
+//#include "CSVTrajectoryManager.h"
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, ros::this_node::getName());
+
+    TrajectoryControlActionServer action_server(ros::this_node::getName());
+
+    ros::spin();
+
+    return 0;
+}
---
+++ References/Lab/exploration_coverage/3dmr/nav_ws/src/robot/nifti_teleop/nodes/laser_rotate.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+rostopic pub /scanning_speed_cmd std_msgs/Float64 1.2
+
